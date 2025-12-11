9ae9394a9df930df7aa9cb312354919058e3aa38
Jhonito021 <jhonito021@gmail.com>
2025-12-12 00:08:41 +0300

    4 commit

--- asset/js/scrip.js
+++ asset/js/scrip.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
             const heartSeal = document.getElementById('heartSeal');
             const letter = document.getElementById('letter');
             const message = document.getElementById('message');
+            const img = document.getElementById("img-bg");
             let isOpen = false;
             
             openBtn.addEventListener('click', function() {
@@ -21,6 +22,11 @@ document.addEventListener('DOMContentLoaded', function() {
                     setTimeout(() => {
                         openBtn.innerHTML = '<i class="fas fa-envelope"></i> Fermer l\'enveloppe';
                     }, 500);
+
+                    // image
+                    setTimeout(() => {
+                        message.classList.add('show');
+                    }, 1500);
                     
                     // Afficher le message romantique
                     setTimeout(() => {
@@ -82,3 +88,17 @@ document.addEventListener('DOMContentLoaded', () => {
         typeWriter(heroTitle, originalText, 100);
     }
 });
+
+function createHeart() {
+    const heart = document.createElement("div");
+    heart.className = "heart";
+    heart.innerHTML = "<i class='fas fa-heart'></i>";
+    heart.style.left = Math.random() * window.innerWidth + "px";
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+      heart.remove();
+    }, 4000);
+  }
+
+  setInterval(createHeart, 300);
